070c0f61dc74eea7fb5b95bd66a96d8b81ba52eb
kaushlendra-2005 <kaushlendrapratapsingh631@gmail.com>
2025-11-06 15:16:17 +0530

    Completed the tower of hanoi

---
+++ DS_LAB/TowerofHanoi.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void TOH(int n,char s,char m,char d){
+    if(n==1){
+        printf("Move disc from %c to %c\n",s,d);
+    }
+    else{
+        TOH(n-1,s,d,m);
+        printf("Move disc from %c to %c\n",s,d);
+        TOH(n-1,m,s,d);
+    }
+}
+
+int main(){
+
+    int n;
+    printf("Enter number of discs: ");
+    scanf("%d",&n);
+    TOH(n,'s','m','d');
+
+    return 0;
+}
